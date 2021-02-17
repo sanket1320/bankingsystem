8aedbb668d7bd89be61ceb21ff854961d4f1e013
sanket1320 <79193011+sanket1320@users.noreply.github.com>
2021-02-17 14:26:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+	
+    <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+		
+	}
+	tr{
+		text-align:center;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("bank-with-coins.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+
+	<div id="header">
+       <br>
+	   <br>
+	   <br>
+	   <br>
+	   <br>
+	   <h2 style=" font-family:Agency FB; font-size: 50px;color:#ff1493;text-shadow: 2px 2px black;"> Spark Foundation Internship Project </h2>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#ff1493;text-shadow: 2px 2px black;"> Basic Banking System </h2>
+	   <h3> By Sanket Kadkol </h3>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+				<button class="button button1" type="button" href="getdetail.php">View Users</button>
+				</a>
+				<br><br>
+				<a href="transaction.php">
+                <button class="button button2" type="button" href="transaction.php">Tansaction History</button>
+				</a>
+				
+               </tr>
+
+            </table>
+    </div>
+</body>
+</html>
